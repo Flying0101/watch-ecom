9ad547aeddd088a0b872898f70d30cfc8fcb8c4b
Flying0101 <stephano@live.se>
2024-04-25 21:33:10 +0200

    add new components into landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Hero from "./components/Hero/Hero";
 import IntroText from "./components/IntroText/IntroText";
+import OurLatest from "./components/OurLatest/OurLatest";
+import Quality from "./components/Quality/Quality";
+import Manufacture from "./components/Manufacture/Manufacture";
+import Footer from "./components/Footer/Footer";
+
 export default function Home() {
   return (
     <main>
       <Hero />
       <IntroText />
+      <OurLatest />
+      <Quality />
+      <Manufacture />
+      <Footer />
     </main>
   );
 }
