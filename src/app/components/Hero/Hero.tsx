b613fa370fd6f01bd9392f1e4c25e1df65711633
Flying0101 <stephano@live.se>
2024-04-25 21:32:52 +0200

    formatting

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <source src="/mp4/hero-video.mp4" type="video/mp4" />
       </video>
 
-      <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 text-white w-11/12  ">
+      <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 text-white w-11/12">
         <h1 className="text-5xl mb-3 mx-auto w-fit">Experience our heritage</h1>
         <p className="text-2xl mx-auto w-fit">
           watches made with scrupulous attention to detail
