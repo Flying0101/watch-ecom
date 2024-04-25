6ba48f710dd949f8ae4519ccd80771507faf324b
Flying0101 <stephano@live.se>
2024-04-25 21:32:13 +0200

    add manufacture component

---
+++ src/app/components/Manufacture/Manufacture.tsx
@@ -0,0 +1,22 @@
+
+const Manufacture = () => {
+  return (
+    <section className="relative pt-14 bg-custom-image bg-cover bg-center h-[200vh] md:h-[150vh]">
+      <div className="max-w-screen-xl mx-auto px-5">
+        <h2 className="text-5xl w-full text-center mb-6 text-white leading-[5rem]">
+          MADE TO BE <br /> TOUGHEST
+        </h2>
+        <p className="text-lg text-center max-w-xl mx-auto text-white leading-10">
+          The process starts during the design phase of a new product. A list is
+          drawn up of the stresses that the watch is likely to have to
+          withstand. Purely virtual and based on a product-use scenario, this
+          first step provides a broad overview of the risks, which are then
+          organized into the order in which the issues should be approached.
+          Simulations can then be programmed.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Manufacture;
