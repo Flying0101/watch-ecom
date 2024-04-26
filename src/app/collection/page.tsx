6f2b8dfc1893e686fbe101924e6174350c63f9db
Flying0101 <stephano@live.se>
2024-04-26 17:51:32 +0200

    update props

---
+++ src/app/collection/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "../components/Hero/Hero";
+
+export default function collection() {
+  return (
+    <main>
+      <Hero title="DISCOVER THE UNKOWN" text="THE COLLECTION" path="/mp4/collection-intro.mp4" />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,16 @@ import Footer from "./components/Footer/Footer";
 export default function Home() {
   return (
     <main>
-      <Hero path="/mp4/hero-video.mp4" />
+      <Hero title="Experience our heritage" text="watches made with scrupulous attention to detail" path="/mp4/hero-video.mp4" />
       <IntroText />
       <OurLatest />
       <Quality />
       <Manufacture />
-      <CTA title="See our whole collection here" ctaText="EXPLORE MORE" path="404"/>
+      <CTA
+        title="See our whole collection here"
+        ctaText="EXPLORE MORE"
+        path="404"
+      />
       <Footer />
     </main>
   );
