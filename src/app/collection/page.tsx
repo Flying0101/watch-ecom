43d9b4b4d4f98767c4d3aac039b2b00f04e0917a
Flying0101 <stephano@live.se>
2024-04-29 17:37:22 +0200

    add new component

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "../components/Hero/Hero";
+import CollectionOverview from "../components/CollectionOverview/CollectionOverview";
 
 export default function collection() {
   return (
     <main>
-      <Hero title="DISCOVER THE UNKOWN" text="THE COLLECTION" path="/mp4/collection-intro.mp4" />
+      <Hero
+        title="DISCOVER THE UNKOWN"
+        text="THE COLLECTION"
+        path="/mp4/collection-intro.mp4"
+      />
+      <CollectionOverview />
     </main>
   );
 }
