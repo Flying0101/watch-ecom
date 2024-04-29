17412e157135cf220839cf04bdb7a5ea9208df13
Flying0101 <stephano@live.se>
2024-04-29 17:44:52 +0200

    test grid with collectionCard

--- src/app/components/CollectionGrid/CollectionGrid.tsx
+++ src/app/components/CollectionGrid/CollectionGrid.tsx
@@ -1,4 +1,124 @@
+import Image from "next/image";
+
 const CollectionGrid = () => {
-  return <div></div>;
+  return (
+    <div className="w-full grid grid-cols-3 gap-4">
+
+
+       <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
+        <div className="h-fit">
+          <Image
+            width={421}
+            height={593}
+            alt="watch"
+            src="/images/rolex-kermit.png"
+            className="w-full px-28 py-8 group-hover:px-24"
+          />
+          <p className="text-gray-700 group-hover:hidden">
+            26393OR.OO.A348KB.01
+          </p>
+          <h2 className="text-xl font-semibold">
+            Code 11.59 by Audemars Piguet
+          </h2>
+          <p className="text-lg">Selfwinding chronograph</p>
+          <p className="text-gray-700 group-hover:hidden">
+            41mm, 18-karat white gold
+          </p>
+        </div>
+      </div>
+
+
+      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
+        <div className="h-fit">
+          <Image
+            width={421}
+            height={593}
+            alt="watch"
+            src="/images/rolex-kermit.png"
+            className="w-full px-28 py-8 group-hover:px-24"
+          />
+          <p className="text-gray-700 group-hover:hidden">
+            26393OR.OO.A348KB.01
+          </p>
+          <h2 className="text-xl font-semibold">
+            Code 11.59 by Audemars Piguet
+          </h2>
+          <p className="text-lg">Selfwinding chronograph</p>
+          <p className="text-gray-700 group-hover:hidden">
+            41mm, 18-karat white gold
+          </p>
+        </div>
+      </div>
+      
+
+      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
+        <div className="h-fit">
+          <Image
+            width={421}
+            height={593}
+            alt="watch"
+            src="/images/rolex-kermit.png"
+            className="w-full px-28 py-8 group-hover:px-24"
+          />
+          <p className="text-gray-700 group-hover:hidden">
+            26393OR.OO.A348KB.01
+          </p>
+          <h2 className="text-xl font-semibold">
+            Code 11.59 by Audemars Piguet
+          </h2>
+          <p className="text-lg">Selfwinding chronograph</p>
+          <p className="text-gray-700 group-hover:hidden">
+            41mm, 18-karat white gold
+          </p>
+        </div>
+      </div>
+      
+
+      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
+        <div className="h-fit">
+          <Image
+            width={421}
+            height={593}
+            alt="watch"
+            src="/images/rolex-kermit.png"
+            className="w-full px-28 py-8 group-hover:px-24"
+          />
+          <p className="text-gray-700 group-hover:hidden">
+            26393OR.OO.A348KB.01
+          </p>
+          <h2 className="text-xl font-semibold">
+            Code 11.59 by Audemars Piguet
+          </h2>
+          <p className="text-lg">Selfwinding chronograph</p>
+          <p className="text-gray-700 group-hover:hidden">
+            41mm, 18-karat white gold
+          </p>
+        </div>
+      </div>
+      
+
+      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
+        <div className="h-fit">
+          <Image
+            width={421}
+            height={593}
+            alt="watch"
+            src="/images/rolex-kermit.png"
+            className="w-full px-28 py-8 group-hover:px-24"
+          />
+          <p className="text-gray-700 group-hover:hidden">
+            26393OR.OO.A348KB.01
+          </p>
+          <h2 className="text-xl font-semibold">
+            Code 11.59 by Audemars Piguet
+          </h2>
+          <p className="text-lg">Selfwinding chronograph</p>
+          <p className="text-gray-700 group-hover:hidden">
+            41mm, 18-karat white gold
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default CollectionGrid;
