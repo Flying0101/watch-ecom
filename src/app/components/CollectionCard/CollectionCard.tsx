8f00b4dc6197eb3ade5c19586d97946852fe0ff0
Flying0101 <stephano@live.se>
2024-05-01 23:24:58 +0200

    update responsivness

--- src/app/components/CollectionCard/CollectionCard.tsx
+++ src/app/components/CollectionCard/CollectionCard.tsx
@@ -1,9 +1,25 @@
+import Image from "next/image";
+
 const CollectionCard = () => {
-
-
-    return(<div>
-        
-    </div>)
+  return (
+    <div className="w-full flex items-center group min-h-[500px] text-center max-h-[700px] bg-[#f6f5f3]">
+      <div className="h-fit mx-auto">
+        <Image
+          width={421}
+          height={593}
+          alt="watch"
+          src="/images/rolex-kermit.png"
+          className="mx-auto w-[10rem] py-8 group-hover:w-[12rem] group-hover:mb-4"
+        />
+        <p className="text-gray-700 group-hover:hidden">26393OR.OO.A348KB.01</p>
+        <h2 className="text-xl font-semibold">Code 11.59 by Audemars Piguet</h2>
+        <p className="text-lg">Selfwinding chronograph</p>
+        <p className="text-gray-700 group-hover:hidden">
+          41mm, 18-karat white gold
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CollectionCard;
--- src/app/components/CollectionGrid/CollectionGrid.tsx
+++ src/app/components/CollectionGrid/CollectionGrid.tsx
@@ -1,122 +1,19 @@
-import Image from "next/image";
+import CollectionCard from "../CollectionCard/CollectionCard";
 
 const CollectionGrid = () => {
+  //load indexed data, state or context?
   return (
-    <div className="w-full grid grid-cols-3 gap-4">
-
-
-       <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
-        <div className="h-fit">
-          <Image
-            width={421}
-            height={593}
-            alt="watch"
-            src="/images/rolex-kermit.png"
-            className="w-full px-28 py-8 group-hover:px-24"
-          />
-          <p className="text-gray-700 group-hover:hidden">
-            26393OR.OO.A348KB.01
-          </p>
-          <h2 className="text-xl font-semibold">
-            Code 11.59 by Audemars Piguet
-          </h2>
-          <p className="text-lg">Selfwinding chronograph</p>
-          <p className="text-gray-700 group-hover:hidden">
-            41mm, 18-karat white gold
-          </p>
-        </div>
-      </div>
-
-
-      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
-        <div className="h-fit">
-          <Image
-            width={421}
-            height={593}
-            alt="watch"
-            src="/images/rolex-kermit.png"
-            className="w-full px-28 py-8 group-hover:px-24"
-          />
-          <p className="text-gray-700 group-hover:hidden">
-            26393OR.OO.A348KB.01
-          </p>
-          <h2 className="text-xl font-semibold">
-            Code 11.59 by Audemars Piguet
-          </h2>
-          <p className="text-lg">Selfwinding chronograph</p>
-          <p className="text-gray-700 group-hover:hidden">
-            41mm, 18-karat white gold
-          </p>
-        </div>
-      </div>
-      
-
-      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
-        <div className="h-fit">
-          <Image
-            width={421}
-            height={593}
-            alt="watch"
-            src="/images/rolex-kermit.png"
-            className="w-full px-28 py-8 group-hover:px-24"
-          />
-          <p className="text-gray-700 group-hover:hidden">
-            26393OR.OO.A348KB.01
-          </p>
-          <h2 className="text-xl font-semibold">
-            Code 11.59 by Audemars Piguet
-          </h2>
-          <p className="text-lg">Selfwinding chronograph</p>
-          <p className="text-gray-700 group-hover:hidden">
-            41mm, 18-karat white gold
-          </p>
-        </div>
-      </div>
-      
-
-      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
-        <div className="h-fit">
-          <Image
-            width={421}
-            height={593}
-            alt="watch"
-            src="/images/rolex-kermit.png"
-            className="w-full px-28 py-8 group-hover:px-24"
-          />
-          <p className="text-gray-700 group-hover:hidden">
-            26393OR.OO.A348KB.01
-          </p>
-          <h2 className="text-xl font-semibold">
-            Code 11.59 by Audemars Piguet
-          </h2>
-          <p className="text-lg">Selfwinding chronograph</p>
-          <p className="text-gray-700 group-hover:hidden">
-            41mm, 18-karat white gold
-          </p>
-        </div>
-      </div>
-      
-
-      <div className=" flex items-center group min-h-[550px] text-center max-h-[700px] bg-[#f6f5f3]">
-        <div className="h-fit">
-          <Image
-            width={421}
-            height={593}
-            alt="watch"
-            src="/images/rolex-kermit.png"
-            className="w-full px-28 py-8 group-hover:px-24"
-          />
-          <p className="text-gray-700 group-hover:hidden">
-            26393OR.OO.A348KB.01
-          </p>
-          <h2 className="text-xl font-semibold">
-            Code 11.59 by Audemars Piguet
-          </h2>
-          <p className="text-lg">Selfwinding chronograph</p>
-          <p className="text-gray-700 group-hover:hidden">
-            41mm, 18-karat white gold
-          </p>
-        </div>
+    <div>
+      <h2 className="mb-6 text-2xl text-gray-800">
+        CODE 11.59 BY AUDEMARS PIGUET
+      </h2>
+      <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
       </div>
     </div>
   );
