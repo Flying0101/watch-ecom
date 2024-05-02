f237deb8bd4b043f44ce678549efd970661cefba
Flying0101 <stephano@live.se>
2024-05-02 11:49:37 +0200

    add more grid cols

--- src/app/components/CollectionGrid/CollectionGrid.tsx
+++ src/app/components/CollectionGrid/CollectionGrid.tsx
@@ -7,7 +7,7 @@ const CollectionGrid = () => {
       <h2 className="mb-6 text-2xl text-gray-800">
         CODE 11.59 BY AUDEMARS PIGUET
       </h2>
-      <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4 gap-4">
         <CollectionCard />
         <CollectionCard />
         <CollectionCard />
