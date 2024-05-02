7d9586f8e8d7456e95cca6453f9a4fea699db302
Flying0101 <stephano@live.se>
2024-05-02 11:49:49 +0200

    update styling for smaller and bigger screens

--- src/app/components/CollectionOverview/CollectionOverview.tsx
+++ src/app/components/CollectionOverview/CollectionOverview.tsx
@@ -2,7 +2,7 @@ import CollectionGrid from "../CollectionGrid/CollectionGrid";
 
 const CollectionOverview = () => {
   return (
-    <section className="max-w-screen-xl mx-auto px-5 py-9 md:py-20">
+    <section className="max-w-screen-xxl mx-auto px-5 py-9 md:py-20 lg:px-10">
         <p>SEARCH FUNCTION HERE</p>
       <CollectionGrid />
     </section>
