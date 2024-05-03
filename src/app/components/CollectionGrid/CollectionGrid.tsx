b72b3801c5a96b33f7e26eedd422a4d2553bacc4
Flying0101 <stephano@live.se>
2024-05-03 23:22:51 +0200

    cleanup and research

--- src/app/components/CollectionGrid/CollectionGrid.tsx
+++ src/app/components/CollectionGrid/CollectionGrid.tsx
@@ -1,7 +1,6 @@
 import CollectionCard from "../CollectionCard/CollectionCard";
 
 const CollectionGrid = () => {
-  //load indexed data, state or context?
   return (
     <div>
       <h2 className="mb-6 text-2xl text-gray-800">
