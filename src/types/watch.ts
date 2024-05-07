c5583db367c2ed5ad96c6025dd4cdf14b0e616bb
Flying0101 <stephano@live.se>
2024-05-07 22:26:57 +0200

    add type for watch object

---
+++ src/types/watch.ts
@@ -0,0 +1,27 @@
+export interface Watch {
+  id: number;
+  brand: string;
+  bezel: string;
+  bracelet: string;
+  clasp: string;
+  dial: string;
+  diameter: string;
+  glass: string;
+  img: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL: string;
+    blurWidth: number;
+  };
+  info: string;
+  material: string;
+  model: string;
+  price: number;
+  reserv: string;
+  resistance: string;
+  serial: string;
+  serie: string;
+  sort: string;
+  variant: string;
+}
