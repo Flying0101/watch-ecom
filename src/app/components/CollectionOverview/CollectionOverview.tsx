9ef3db8ddc918282893ce4859281fa9d90cfe36a
Flying0101 <stephano@live.se>
2024-05-07 22:28:24 +0200

    update and add data from context

--- src/app/components/CollectionOverview/CollectionOverview.tsx
+++ src/app/components/CollectionOverview/CollectionOverview.tsx
@@ -1,10 +1,14 @@
+'use client'
 import CollectionGrid from "../CollectionGrid/CollectionGrid";
+import { useSearch } from "@/app/contexts/WatchContext";
 
 const CollectionOverview = () => {
+  const {allWatches, loading} = useSearch();
+  
   return (
     <section className="max-w-screen-xxl mx-auto px-5 py-9 md:py-20 lg:px-10">
-        <p>SEARCH FUNCTION HERE</p>
-      <CollectionGrid />
+      <p>SEARCH FUNCTION HERE</p>
+      <CollectionGrid data={allWatches} loading={loading} />
     </section>
   );
 };
