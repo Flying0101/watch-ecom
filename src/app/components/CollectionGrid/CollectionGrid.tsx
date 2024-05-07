deae1adede2abd139b8b4251cb09f1aa5987aee0
Flying0101 <stephano@live.se>
2024-05-07 22:28:02 +0200

    update and type, make more dynamic

--- src/app/components/CollectionGrid/CollectionGrid.tsx
+++ src/app/components/CollectionGrid/CollectionGrid.tsx
@@ -1,18 +1,28 @@
 import CollectionCard from "../CollectionCard/CollectionCard";
+import { WatchCollection } from "@/types/watchCollection";
+import { Watch } from "@/types/watch";
 
-const CollectionGrid = () => {
+
+const CollectionGrid = ({
+  data,
+  loading,
+}: {
+  data: WatchCollection;
+  loading: boolean;
+}) => {
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  console.log(data)
   return (
     <div>
       <h2 className="mb-6 text-2xl text-gray-800">
         CODE 11.59 BY AUDEMARS PIGUET
       </h2>
       <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4 gap-4">
-        <CollectionCard />
-        <CollectionCard />
-        <CollectionCard />
-        <CollectionCard />
-        <CollectionCard />
-        <CollectionCard />
+       {data.collection.map((item: Watch) => (
+          <CollectionCard key={item.id} watch={item} />
+        ))}
       </div>
     </div>
   );
