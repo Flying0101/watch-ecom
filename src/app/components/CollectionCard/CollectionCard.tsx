7e28fd77e85666614b7aacf557f4f3b70ce404c8
Flying0101 <stephano@live.se>
2024-05-07 22:26:39 +0200

    type collectionCard and add prop

--- src/app/components/CollectionCard/CollectionCard.tsx
+++ src/app/components/CollectionCard/CollectionCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-
-const CollectionCard = () => {
+import { Watch } from "@/types/watch";
+//type later
+const CollectionCard = ({ watch }: { watch: Watch }) => {
   return (
     <div className="w-full flex items-center group min-h-[500px] text-center max-h-[700px] bg-[#f6f5f3] hover:cursor-pointer">
       <div className="h-fit mx-auto">
@@ -12,14 +13,14 @@ const CollectionCard = () => {
           className="mx-auto w-[10rem] py-8 transition-all duration-500 ease-in-out group-hover:w-[12rem] group-hover:mb-4"
         />
         <p className="px-3 text-sm lg:text-base text-gray-700 duration-500 ease-in-out group-hover:opacity-0">
-          26393OR.OO.A348KB.01
+          {watch.serial}
         </p>
         <h2 className="px-3 text-base lg:text-xl font-semibold">
-          Code 11.59 by Audemars Piguet
+          {watch.brand}
         </h2>
-        <p className="px-3 text-sm lg:text-lg">Selfwinding chronograph</p>
+        <p className="px-3 text-sm lg:text-lg">{watch.variant}</p>
         <p className="px-3 text-sm lg:text-base text-gray-700 font-light duration-500 ease-in-out group-hover:opacity-0">
-          41mm, 18-karat white gold
+          {watch.material}
         </p>
       </div>
     </div>
