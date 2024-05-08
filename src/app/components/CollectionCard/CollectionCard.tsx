ea452f87e3311c5e6b55aa04f95cd48b43debd42
Flying0101 <stephano@live.se>
2024-05-08 17:26:28 +0200

    use mockdata instead of constant values

--- src/app/components/CollectionCard/CollectionCard.tsx
+++ src/app/components/CollectionCard/CollectionCard.tsx
@@ -6,17 +6,17 @@ const CollectionCard = ({ watch }: { watch: Watch }) => {
     <div className="w-full flex items-center group min-h-[500px] text-center max-h-[700px] bg-[#f6f5f3] hover:cursor-pointer">
       <div className="h-fit mx-auto">
         <Image
-          width={421}
-          height={593}
+          width={watch.img.width}
+          height={watch.img.height}
           alt="watch"
-          src="/images/rolex-kermit.png"
+          src={watch.img}
           className="mx-auto w-[10rem] py-8 transition-all duration-500 ease-in-out group-hover:w-[12rem] group-hover:mb-4"
         />
         <p className="px-3 text-sm lg:text-base text-gray-700 duration-500 ease-in-out group-hover:opacity-0">
           {watch.serial}
         </p>
         <h2 className="px-3 text-base lg:text-xl font-semibold">
-          {watch.brand}
+          {watch.model}
         </h2>
         <p className="px-3 text-sm lg:text-lg">{watch.variant}</p>
         <p className="px-3 text-sm lg:text-base text-gray-700 font-light duration-500 ease-in-out group-hover:opacity-0">
