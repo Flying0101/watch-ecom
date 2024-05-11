7023aec4ee5497583835cd4af3c02fae86931f52
Flying0101 <stephano@live.se>
2024-05-11 21:11:52 +0200

    add search field component

---
+++ src/app/components/SearchField/SearchField.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { IconX, IconSearch } from "@tabler/icons-react";
+import { useSearch } from "@/app/contexts/WatchContext";
+
+const SearchField = () => {
+  const { HandleFocus} = useSearch();
+
+  return (
+    <div className="w-full flex relative">
+      <input
+        type="text"
+        placeholder="Search after your brand"
+        className="min-h-16 text-5xl border-b border-black w-full mb-16 outline-none"
+        onFocus={() => {
+          HandleFocus();
+        }}
+      />
+        <IconX
+          className="absolute right-5 w-12 h-12 hover:text-gray-500 hover:cursor-pointer"
+          stroke={1}
+        />
+        <IconSearch
+          className="absolute right-5 w-12 h-12 hover:text-gray-500 hover:cursor-pointer"
+          stroke={1}
+        />
+    </div>
+  );
+};
+
+export default SearchField;
