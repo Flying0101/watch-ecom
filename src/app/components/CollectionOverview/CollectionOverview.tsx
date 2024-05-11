e16156b23548b4d0ba95144ff0e63762bc735882
Flying0101 <stephano@live.se>
2024-05-11 21:11:42 +0200

    turn search input to component

--- src/app/components/CollectionOverview/CollectionOverview.tsx
+++ src/app/components/CollectionOverview/CollectionOverview.tsx
@@ -1,14 +1,15 @@
-'use client'
+"use client";
 import CollectionGrid from "../CollectionGrid/CollectionGrid";
 import { useSearch } from "@/app/contexts/WatchContext";
+import SearchField from "../SearchField/SearchField";
 
 const CollectionOverview = () => {
-  const {allWatches, loading} = useSearch();
-  
+  const { allWatches, loading } = useSearch();
+
   return (
     <section className="max-w-screen-xxl mx-auto px-5 py-9 md:py-20 lg:px-10">
-      <p>SEARCH FUNCTION HERE</p>
-      <CollectionGrid data={allWatches} loading={loading} />
+      <SearchField />
+      <CollectionGrid data={allWatches} loading={loading} defaultTitle={true} />
     </section>
   );
 };
