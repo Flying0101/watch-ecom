ff5195eec2aba08e78ed5fa08bb1038a4b98e2ce
Flying0101 <stephano@live.se>
2024-05-12 15:21:48 +0200

    update filed make it more interactive

--- src/app/components/SearchField/SearchField.tsx
+++ src/app/components/SearchField/SearchField.tsx
@@ -3,27 +3,39 @@ import { IconX, IconSearch } from "@tabler/icons-react";
 import { useSearch } from "@/app/contexts/WatchContext";
 
 const SearchField = () => {
-  const { HandleFocus, setSearchWord } = useSearch();
+  const { HandleFocus, setSearchWord, setResults, searchWord } = useSearch();
+
+  const resetSearchWord = () => {
+    setResults([]);
+    setSearchWord("");
+  };
 
   return (
     <div className="w-full flex relative">
       <input
         type="text"
-        placeholder="Search after your brand"
-        className="min-h-16 text-5xl border-b border-black w-full mb-16 outline-none"
+        value={searchWord}
+        placeholder="Search after brand"
+        className="min-h-16 text-5xl border-b border-black w-full mb-16 outline-none font-thin text-black"
         onFocus={() => {
           HandleFocus();
         }}
         onChange={(e) => setSearchWord(e.target.value)}
       />
-      <IconX
-        className="absolute right-5 w-12 h-12 hover:text-gray-500 hover:cursor-pointer"
-        stroke={1}
-      />
-      <IconSearch
-        className="absolute right-5 w-12 h-12 hover:text-gray-500 hover:cursor-pointer"
-        stroke={1}
-      />
+      {searchWord.length > 0 ? (
+        <IconX
+          onClick={() => {
+            resetSearchWord();
+          }}
+          className="absolute right-5 w-12 h-12 hover:text-gray-500 hover:cursor-pointer"
+          stroke={1}
+        />
+      ) : (
+        <IconSearch
+          className="absolute right-5 w-12 h-12 hover:text-gray-500 hover:cursor-pointer"
+          stroke={1}
+        />
+      )}
     </div>
   );
 };
