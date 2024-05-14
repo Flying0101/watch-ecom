f411d9701d387f1ef5f22a510ede465511565164
Flying0101 <stephano@live.se>
2024-05-14 20:15:09 +0200

    update state name for checkedinput

--- src/app/components/Filtering/CheckedInput/CheckedInput.tsx
+++ src/app/components/Filtering/CheckedInput/CheckedInput.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const CheckedInput = ({title, lbl, checkedValue, setCheckedValue}: {title: string, lbl: string, checkedValue: any, setCheckedValue: any}) => {
+const CheckedInput = ({title, lbl, checkedSize, setCheckedSize}: {title: string, lbl: string, checkedSize: any, setCheckedSize: any}) => {
 
   
 
@@ -8,7 +8,7 @@ const CheckedInput = ({title, lbl, checkedValue, setCheckedValue}: {title: strin
 
   return (
     <div className="py-3 mr-10 flex items-center min-w-28">
-      <input onChange={()=>{setCheckedValue(lbl)}} type="checkbox" id={`${lbl}`} className="min-w-6 min-h-6 mr-2 hover:cursor-pointer" checked={checkedValue === lbl } />
+      <input onChange={()=>{setCheckedSize(lbl)}} type="checkbox" id={`${lbl}`} className="min-w-6 min-h-6 mr-2 hover:cursor-pointer" checked={checkedSize === lbl } />
       <label htmlFor={`${lbl}`} className="hover:cursor-pointer">
         {title}
       </label>
