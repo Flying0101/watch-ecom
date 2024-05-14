146e30d92bfd01ee66c67be73de78c2aec880ec6
Flying0101 <stephano@live.se>
2024-05-14 20:09:48 +0200

    add close button and update component

--- src/app/components/Filtering/Filtering.tsx
+++ src/app/components/Filtering/Filtering.tsx
@@ -4,8 +4,8 @@ import { useSearch } from "@/app/contexts/WatchContext";
 import Image from "next/image";
 
 const Filtering = () => {
-  const { activeFilter, checkedValue, setCheckedValue } = useSearch();
-
+  const { activeFilter, checkedValue, setCheckedValue, setActiveFilter, checkedMaterial, setCheckedMaterial } =
+    useSearch();
 
   return (
     <div>
@@ -21,45 +21,96 @@ const Filtering = () => {
           <FilterButton title="Material" />
         </div>
       </div>
-      {activeFilter === "Material" && (
-        <div className="w-full border-b-2 border-b-black mb-7 pb-10">
-          <p className="font-thin text-3xl mb-3">CASES</p>
-          <div className="flex">
-            <div className="py-3 max-w-[90px] mr-5">
-              <Image
-                alt="material"
-                src="/material/gold-material.png"
-                width={1000}
-                height={1000}
-                className="w-20 h-20 mb-2"
-              />
-              <p>18-CARAT GOLD</p>
+
+      <div
+        className={
+          activeFilter.length > 0
+            ? "relative w-full border-b-2 border-b-black mb-7 pb-10"
+            : "hidden"
+        }
+      >
+        <button
+          onClick={() => {
+            setActiveFilter("");
+          }}
+          className="absolute bottom-0  right-0 px-20 py-3 bg-black text-white text-sm"
+        >
+          DONE
+        </button>
+
+        {activeFilter === "Material" && (
+          <div>
+            <p className="font-thin text-3xl mb-3">CASES</p>
+            <div className="flex">
+              <div className="py-3 max-w-[90px] mr-5 hover:cursor-pointer"  onClick={()=>{setCheckedMaterial('18-CARAT GOLD')}}>
+                <Image
+                  alt="material"
+                  src="/material/gold-material.png"
+                  width={1000}
+                  height={1000}
+                  className="w-20 h-20 mb-2"
+                />
+                <p>18-CARAT GOLD</p>
+              </div>
+              <div className="py-3 max-w-[90px] hover:cursor-pointer" onClick={()=>{setCheckedMaterial('Stainless steel')}}>
+                <Image
+                  alt="material"
+                  src="/material/steel-material.png"
+                  width={1000}
+                  height={1000}
+                  className="w-20 h-20 mb-2"
+                />
+                <p>Stainless Steel</p>
+              </div>
             </div>
-            <div className="py-3 max-w-[90px]">
-              <Image
-                alt="material"
-                src="/material/steel-material.png"
-                width={1000}
-                height={1000}
-                className="w-20 h-20 mb-2"
+            <button
+              onClick={() => {
+                setActiveFilter("");
+              }}
+              className="absolute bottom-0  right-0 px-20 py-3 bg-black text-white text-sm"
+            >
+              DONE
+            </button>
+          </div>
+        )}
+        {activeFilter === "Size" && (
+          <div>
+            <p className="font-thin text-3xl mb-3">CASE DIAMETER</p>
+            <div className="flex flex-wrap w-full">
+              <CheckedInput
+                title="43 mm"
+                lbl="43"
+                checkedValue={checkedValue}
+                setCheckedValue={setCheckedValue}
               />
-              <p>Stainless Steel</p>
+              <CheckedInput
+                title="42 mm"
+                lbl="42"
+                checkedValue={checkedValue}
+                setCheckedValue={setCheckedValue}
+              />
+              <CheckedInput
+                title="41 mm"
+                lbl="41"
+                checkedValue={checkedValue}
+                setCheckedValue={setCheckedValue}
+              />
+              <CheckedInput
+                title="40 mm"
+                lbl="40"
+                checkedValue={checkedValue}
+                setCheckedValue={setCheckedValue}
+              />
+              <CheckedInput
+                title="39 mm"
+                lbl="39"
+                checkedValue={checkedValue}
+                setCheckedValue={setCheckedValue}
+              />
             </div>
           </div>
-        </div>
-      )}
-      {activeFilter === "Size" && (
-        <div className="w-full border-b-2 border-b-black mb-7 pb-10">
-          <p className="font-thin text-3xl mb-3">CASE DIAMETER</p>
-          <div className="flex flex-wrap w-full">
-          <CheckedInput title="43 mm" lbl="43" checkedValue={checkedValue} setCheckedValue={setCheckedValue} />
-          <CheckedInput title="42 mm" lbl="42" checkedValue={checkedValue} setCheckedValue={setCheckedValue}/>
-          <CheckedInput title="41 mm" lbl="41" checkedValue={checkedValue} setCheckedValue={setCheckedValue}/>
-          <CheckedInput title="40 mm" lbl="40" checkedValue={checkedValue} setCheckedValue={setCheckedValue}/>
-          <CheckedInput title="39 mm" lbl="39" checkedValue={checkedValue} setCheckedValue={setCheckedValue}/>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
