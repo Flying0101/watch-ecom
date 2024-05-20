e83aa71d236a69ee9cbe910de15fb1a24f5560fa
Flying0101 <stephano@live.se>
2024-05-20 16:12:16 +0200

    add some edge cases

--- src/app/collection/[slug]/page.tsx
+++ src/app/collection/[slug]/page.tsx
@@ -3,13 +3,24 @@ import ProductSpecification from "@/app/components/ProductSpecification/ProductS
 
 export default function Page({ params }: { params: { slug: string } }) {
   const decodedSlug = decodeURIComponent(params.slug);
-  const parts = decodedSlug.split('-');
-  const id = parts[parts.length - 1]; 
+  const parts = decodedSlug.split("-");
+  const id = parts[parts.length - 1];
 
+  const idNumber = parseInt(id);
+
+  if (isNaN(idNumber) || idNumber > 16 || idNumber < 0) {
     return (
+      <div className="py-9 min-h-[90vh] flex justify-center items-center">
+        <h2>
+          Error: watch not found. <br /> Please check your URL or retry later..
+        </h2>
+      </div>
+    );
+  }
+  return (
     <section className="py-9 min-h-[90vh]">
       <ProductOverview id={id} />
-      <ProductSpecification id={id}/>
+      <ProductSpecification id={id} />
     </section>
   );
 }
