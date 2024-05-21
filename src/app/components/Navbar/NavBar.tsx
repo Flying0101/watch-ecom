87ece874deefe8c034f7b2edc79e6c07d6557ca3
Flying0101 <stephano@live.se>
2024-05-21 21:47:06 +0200

    add dynamic value to items in cart

--- src/app/components/Navbar/NavBar.tsx
+++ src/app/components/Navbar/NavBar.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useSearch } from "@/app/contexts/WatchContext";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import { IconShoppingCart } from "@tabler/icons-react";
 
 const NavBar = () => {
+  const { cartWatches } = useSearch();
+
   return (
     <nav className="group absolute px-6 w-full left-2/4 -translate-x-1/2 max-w-screen-xl h-20 top-0 backdrop-blur-md text-white transition-all ease-out duration-700 flex z-10 hover:bg-white hover:bg-opacity-50 hover:duration-[0.35s]">
       <div className="md:hidden flex justify-center content-center">
@@ -22,8 +26,14 @@ const NavBar = () => {
         <li className="text-md font-bold tracking-[1px] pr-8 no-underline transition-all ease-out duration-700 group-hover:text-black hover:duration-[0.35s] hover:cursor-pointer">
           <IconShoppingCart className="size-8" />
         </li>
-        <p className="absolute right-4 top-4 bg-blue-400 p-1 rounded-full text-white text-xs group-hover:text-black">
-          50
+        <p
+          className={
+            cartWatches.length > 0
+              ? "absolute right-4 top-4 bg-blue-400 p-1 rounded-full text-white text-xs group-hover:text-black"
+              : "hidden"
+          }
+        >
+          {cartWatches.length}
         </p>
       </ul>
     </nav>
