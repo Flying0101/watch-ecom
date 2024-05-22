20651cdc31ee35b7d9578fd02709a1250513a466
Flying0101 <stephano@live.se>
2024-05-22 17:15:06 +0200

    change to next Link component

--- src/app/components/CTA/CTA.tsx
+++ src/app/components/CTA/CTA.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const CTA = ({
   title,
@@ -12,9 +13,6 @@ const CTA = ({
 }) => {
   const router = useRouter();
 
-  const handleClick = () => {
-    router.push(`/${path}`);
-  };
  
   return (
     <div className="w-full flex justify-center items-center min-h-[300px] my-7 bg-cta-image bg-cover bg-center relative">
@@ -23,13 +21,14 @@ const CTA = ({
         <h2 className="text-center text-3xl mb-5 text-white font-sans">
           {title}
         </h2>
+        <Link href={`${path}`}>
         <button
           type="button"
-          onClick={handleClick}
           className="block mx-auto w-fit py-2 px-9 rounded-full border-white border-x-8 border-y-2 text-white hover:text-black hover:bg-white font-bold hover:px-12 hover:py-3 transition-all duration-500 ease-in-out"
         >
           {ctaText}
         </button>
+        </Link>
       </div>
     </div>
   );
