d87d37c3eb574c07b2dfb95d0c935ddc525759ca
Flying0101 <stephano@live.se>
2024-05-22 16:02:49 +0200

    add component to page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,8 @@
+import Hero from "../components/Hero/Hero";
+import CartOverview from "../components/CartOverview/CartOverview";
+
+export default function cart() {
+  return <main>
+    <CartOverview />
+  </main>;
+}
