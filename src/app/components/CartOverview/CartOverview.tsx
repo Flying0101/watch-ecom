857ceb12958d48b2a3ea95b2c42819d3df431f8f
Flying0101 <stephano@live.se>
2024-05-22 17:15:39 +0200

    move to overview and pass down thru props

--- src/app/components/CartOverview/CartOverview.tsx
+++ src/app/components/CartOverview/CartOverview.tsx
@@ -1,14 +1,18 @@
+'use client'
 import CartList from "../CartList/CartList";
 import CartSummary from "../CartSummary/CartSummary";
+import { useSearch } from "@/app/contexts/WatchContext";
 
 const CartOverview = () => {
+
+    const{cartWatches} = useSearch();       
 
   return (
     <div className="w-full relative max-w-screen-xl mx-auto px-5 mt-28 mb-14 min-h-[80vh]">
       <h1 className="text-3xl font-semibold mt-3 mb-6">Shopping Cart</h1>
       <span className="flex flex-wrap min-h-[70vh]">
-        <CartList />
-        <CartSummary />
+        <CartList cartWatches={cartWatches} />
+        <CartSummary cartWatches={cartWatches}/>
       </span>
     </div>
   );
