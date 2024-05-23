ae111724037e75c1a5a9f403847ca0d3bbd4d6ae
Flying0101 <stephano@live.se>
2024-05-23 18:33:48 +0200

    update with total price

--- src/app/components/CartSummary/CartSummary.tsx
+++ src/app/components/CartSummary/CartSummary.tsx
@@ -2,15 +2,16 @@ import { Watch } from "@/types/watch";
 import { IconChevronLeft } from "@tabler/icons-react";
 import Link from "next/link";
 
-const CartSummary = ({cartWatches}: {cartWatches: Watch[]}) => {
-
-    const totalSum = cartWatches.reduce((acc, watch) => acc + watch.price, 0);
-    return (
+const CartSummary = ({ cartWatches }: { cartWatches: Watch[] }) => {
+  const totalSum = cartWatches.reduce((acc, watch) => acc + watch.total, 0);
+  return (
     <div className="w-full py-20 min-h-[65vh] md:min-h-[0vh] md:py-0 md:w-1/4 border-t border-t-gray-700 md:border-none">
       <div className="max-w-44 h-full flex flex-col justify-between mx-auto">
         <span>
           <h2 className="font-semibold mb-2">Summary</h2>
-          <p className="text-gray-600 font-medium">In your bag {cartWatches?.length} items</p>
+          <p className="text-gray-600 font-medium">
+            In your bag {cartWatches?.length} items
+          </p>
         </span>
         <span>
           <p className="text-gray-600 font-medium">Shipping Cost:</p>
