dfce464ef092b9a74f378829f4d87887e441200e
Flying0101 <stephano@live.se>
2024-05-23 19:08:43 +0200

    This reverts commit 974e92f1bdb72bca48656b55136713ddb58506cd.

--- src/app/components/CartList/CartList.tsx
+++ src/app/components/CartList/CartList.tsx
@@ -1,31 +1,19 @@
-"use client";
+'use client'
 import Image from "next/image";
 import CartItem from "../CartItem/CartItem";
 import { Watch } from "@/types/watch";
-const CartList = ({
-  cartWatches,
-  removeFromCart,
-  removeWatch,
-  addToCart,
-}: {
-  cartWatches: Watch[];
-  removeFromCart: any;
-  removeWatch: any;
-  addToCart: any;
-}) => {
+const CartList = ({cartWatches, removeWatch}: {cartWatches: Watch[], removeWatch: any}) => {
+
+
+
   return (
     <div className="w-full md:w-3/4">
       <span className="flex-col flex md:flex-row flex-wrap h-full">
         <div className="w-fit mx-auto md:mx-0 md:w-full lg:w-3/4">
-          {cartWatches.map((watch: Watch) => (
-            <CartItem
-              key={watch.id}
-              data={watch}
-              removeFromCart={removeFromCart}
-              addToCart={addToCart}
-              removeWatch={removeWatch}
-            />
-          ))}
+            {cartWatches.map((watch: Watch) => (
+                <CartItem key={watch.id} data={watch} removeWatch={removeWatch} />
+            ))}
+           
         </div>
         <div className=" hidden lg:block w-full md:w-1/4 bg-gradient-to-t from-gray-400 to-[#e3e8ea] border-t border-t-[#e3e8ea] ">
           <Image
