9bcb58b5d6b684af33a521e28ba3e3cca87bd8ef
Flying0101 <stephano@live.se>
2024-05-23 18:52:55 +0200

    update props

--- src/app/components/CartOverview/CartOverview.tsx
+++ src/app/components/CartOverview/CartOverview.tsx
@@ -1,18 +1,22 @@
-'use client'
+"use client";
 import CartList from "../CartList/CartList";
 import CartSummary from "../CartSummary/CartSummary";
 import { useSearch } from "@/app/contexts/WatchContext";
 
 const CartOverview = () => {
-
-    const{cartWatches, removeFromCart} = useSearch();       
+  const { cartWatches, removeFromCart, removeWatch, addToCart } = useSearch();
 
   return (
     <div className="w-full relative max-w-screen-xl mx-auto px-5 mt-28 mb-14 min-h-[80vh]">
       <h1 className="text-3xl font-semibold mt-3 mb-6">Shopping Cart</h1>
       <span className="flex flex-wrap min-h-[70vh]">
-        <CartList cartWatches={cartWatches} removeWatch={removeFromCart} />
-        <CartSummary cartWatches={cartWatches}/>
+        <CartList
+          cartWatches={cartWatches}
+          removeFromCart={removeFromCart}
+          removeWatch={removeWatch}
+          addToCart={addToCart}
+        />
+        <CartSummary cartWatches={cartWatches} />
       </span>
     </div>
   );
